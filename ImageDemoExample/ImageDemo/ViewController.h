8b338bcb6e8834f624f53a8bf68ab0bb6edebb2d
lgh <lgh@lgh.local>
2016-12-29 11:31:14 +0800

    初始化项目和库

---
+++ ImageDemoExample/ImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageDemo
+//
+//  Created by lgh on 16/12/29.
+//  Copyright © 2016年 RJone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageDemoExample/ImageDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ImageDemo
+//
+//  Created by lgh on 16/12/29.
+//  Copyright © 2016年 RJone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
